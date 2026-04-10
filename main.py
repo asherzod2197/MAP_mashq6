5c3495778e90d46e3fd39c5a4c7704da44b668d6
Ataxanov <asherzod2197@gmail.com>
2026-04-10 17:39:26 +0500

    Add multiple map examples in main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# 1
+roy = ["hi", "hello"]
+print(list(map(lambda x: len(x) + 1, roy)))
 
+# 2
+roy = [12, 24, 36]
+print(list(map(lambda x: x / 12, roy)))
+
+# 3
+roy = ["A", "B", "C"]
+print(list(map(lambda x: x.lower(), roy)))
+
+# 4
+roy = [3, 5, 7]
+print(list(map(lambda x: str(x * 10), roy)))
+
+# 5
+roy = ["1", "2", "3"]
+print(list(map(lambda x: int(x) + 100, roy)))
